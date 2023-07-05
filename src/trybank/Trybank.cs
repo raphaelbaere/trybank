984c50c41b4ef7d6964e254137aee62b66336d4e
raphaelbaere <raphaelbaere@id.uff.br>
2023-07-05 15:19:07 -0300

    Requisito 7 concluído com sucesso!

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -111,7 +111,18 @@ public class Trybank
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
     public void Transfer(int destinationNumber, int destinationAgency, int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        } 
+        Withdraw(value);
+            for (int i = 0; i < registeredAccounts; i++)
+            {
+                if (Bank[i, 1] == destinationAgency && Bank[i, 0] == destinationNumber)
+                {
+                   Bank[i, 3] += value;
+                }
+            }
     }
 
    
